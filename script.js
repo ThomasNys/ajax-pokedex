4baed0577c37117be25da6fa2c9929dee456bd09
Thomas <ThomasNys1024@gmail.com>
2019-01-17 14:06:21 +0100

    version 1

--- script.js
+++ script.js
@@ -5,32 +5,16 @@ function getPoke(){
     PknSearch=document.getElementById("SearchName").value.toLowerCase();
     var xhr = new XMLHttpRequest();
     var xhr2 = new XMLHttpRequest();
-    var xhr3 = new XMLHttpRequest();
 
     // xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+SearchName+"/, true);
     xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+PknSearch+'/', true);
     xhr2.open('GET', 'https://pokeapi.co/api/v2/pokemon-species/'+PknSearch+'/', true);
-    console.log(xhr2);
+
     xhr2.onload=function(){
         if(this.status == 200){
             var pokemonPre = JSON.parse(this.responseText);
             if (pokemonPre.evolves_from_species!=null){
-                console.log(pokemonPre.evolves_from_species.name)
-                document.getElementById('prevo').innerHTML=pokemonPre.evolves_from_species.name;
-
-                xhr3.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+pokemonPre.evolves_from_species.name+'/', true);
-                xhr3.send();
-                xhr3.onload=function(){
-                    console.log(xhr3);
-                    var pokemonPreI = JSON.parse(this.responseText);
-                    console.log(pokemonPreI);
-                    document.getElementById('pknimg2').src=pokemonPreI.sprites.front_default;
-                }
-            }else{
-            document.getElementById('prevo').innerHTML="no pre-evolution";
-
-            document.getElementById('pknimg2').src="https://imgc.allpostersimages.com/img/Lamination/posters/pokemon-eevee-evolution_a-G-14816840-0.jpg";
-}
+            }
         }
     };
 
@@ -65,5 +49,4 @@ function getPoke(){
     }
 
     xhr.send();
-    xhr2.send();
 };
